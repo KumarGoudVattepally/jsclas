b5bedc2f3fd2d3d9c8ef3ed6d8138c636a1e905f
Kumar Goud Vattepally <kumargoudvattepally3@gmail.com>
2024-10-26 09:46:10 +0530

    re

---
+++ index.js
@@ -0,0 +1,13 @@
+// var ip="JavaSCipt"
+// var em=""
+// for(i in ip){
+//     if(ip[i]==ip[i].toLowerCase()){
+//         em=em+ip[i]
+//     }
+// }
+// console.log(em)
+var str=["good","hi","hello","welcome","students","bye"]
+var str1=""
+for(i in str){
+    
+}

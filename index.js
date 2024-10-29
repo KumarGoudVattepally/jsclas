7b4b88725a292e48386e14e0008c62aa7bba97f9
Kumar Goud Vattepally <kumargoudvattepally3@gmail.com>
2024-10-29 09:59:55 +0530

    js11

--- index.js
+++ index.js
@@ -9,10 +9,34 @@
 // var str=["good","hi","hello","welcome","students","bye"]
 // var str1=""
 // for(i in str){
-    
+
 // }
-var age=18;
-var citizenship = flase;
-(age>=18 && citizwnship)?console.log("hw/she can vote"):console.log("cannot vote");
+// var age = 18;
+// var citizenship = false;
+// age >= 18 && citizenship
+//   ? console.log("hw/she can vote")
+//   : console.log("cannot vote");
 
-
+// var islogin = false;
+// var issignup = true;
+// issignup
+//   ? islogin
+//     ? console.log("ho,e page")
+//     : console.log("login page")
+//   : console.log("create a new ac");
+// var ismale = true;
+// var age = 19;
+// ismale
+//   ? age >= 21
+//     ? console.log("he can marry")
+//     : console.log("he cannot marry")
+//   : age >= 18
+//   ? console.log("she can marry")
+//   : console.log("she cannot marry");
+//   for(i=1;i<=10;i++){
+//     console.log(i);
+//   }
+for(i=1;i<=5;i=i+2){
+    console.log(i);
+    
+}

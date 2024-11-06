8cdb03b210f1fb8f05923a6f8dcd55cd9d99bde2
Kumar Goud Vattepally <kumargoudvattepally3@gmail.com>
2024-11-06 09:58:09 +0530

    functions

---
+++ js13.js
@@ -0,0 +1,59 @@
+// function sum() {
+//   console.log(2 + 2);
+//   return "hello";
+// }
+// console.log(sum());
+// function demo(a,b){
+//     console.log(a+b,"sum");
+//     console.log(a*b,"mul");
+//     console.log(a-b,"sub");
+
+// }
+// demo(3,7);
+// demo(4,7);
+// demo(5,7);
+// demo(6,7);
+
+// function add(){
+//     var a=3;
+//     var b=9;
+//     return a+b;
+// }
+// console.log(add());
+// var x=function(){
+//     console.log("iam kumar");
+
+// }
+// x()
+// var x=function demo(a,b){
+//     console.log("iam kumar");
+//     console.log(a+b);
+
+// }
+// x(5,7)
+// demo();
+// function demo(){
+//     console.log("hello world");
+
+// }
+// demo();
+// function demo(){
+//     console.log("hello world");
+
+// }
+// demo();
+// function loa(x) {
+//   // console.log(x.length);
+//   console.log(x[0] + x[x.length - 1]);
+// }
+// loa([1, 4, 5, 6, 7, 8]);
+//  loa([1,3,4,44])
+// function jio(x) {
+//   if (x.length % 2 == 0) {
+//     console.log("even number");
+//   } else {
+//     console.log("odd number");
+//   }
+// }
+// jio([1, 2, 3, 4, 5]);
+// jio([4, 5, 6, 7]);

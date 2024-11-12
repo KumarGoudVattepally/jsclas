a60c09eb925aae82d89a0e70351faa8d1daf02bd
Kumar Goud Vattepally <kumargoudvattepally3@gmail.com>
2024-11-12 08:29:09 +0530

    scope

--- js15.js
+++ js15.js
@@ -1,4 +1,37 @@
-var sdt=(a)=>a;
-console.log(std("kumar"));
-console.log(std("mahesh"));
+// var sdt=(a)=>{return a};
+// console.log(sdt("kumar"));
+// console.log(sdt("mahesh"));
 
+// var sum=(a,b)=>{console.log(a+b)};
+// var sum=(a,b)=>a+b;
+// console.log(sum(4,5));
+
+// var mul=(a,b)=>{ 
+//     var arr=[];
+//     for(i=1;i<=b;i++){
+//         arr.push(a*i)
+//     }
+//     // console.log(arr);
+//     return arr;
+    
+// };
+// console.log((mul(2,6)));
+// if(true){
+//     var a=40;
+// }
+// console.log(a);
+// var a=40;
+// if(true){
+//     console.log(a);
+// }
+// // console.log(a);
+//*************************************************************************************
+// function outerfun(){
+//     var x=40;
+//     function innerfun(){
+//         console.log(x);
+        
+//     }
+//     innerfun();
+// }
+// outerfun();
